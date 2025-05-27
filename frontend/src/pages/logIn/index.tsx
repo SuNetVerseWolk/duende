0adfb951248765ff84ec629dd164191d2777860c
SuNetVerseWolk <volk333333333@gmail.com>
2025-05-27 12:40:12 +0300

    demo auth

--- frontend/src/pages/logIn/index.tsx
+++ frontend/src/pages/logIn/index.tsx
@@ -1,23 +1,51 @@
 import Link from "next/link";
-import { FormEvent } from "react";
+import { ChangeEvent, FormEvent, useState } from "react";
 import "../../app/globals.css";
+import { supabase } from "@/lib/supabaseClient";
+import { useRouter } from "next/navigation";
 
 export default function LogIn() {
-  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+  const router = useRouter();
+  const [formData, setFromData] = useState({
+    email: "",
+    password: "",
+  });
+
+  const handleInputChange = (e: ChangeEvent<HTMLInputElement>) =>
+    setFromData((prev) => ({ ...prev, [e.target.name]: e.target.value }));
+
+  const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
+
+    const { error, data } = await supabase.auth.signInWithPassword({
+      email: formData.email,
+      password: formData.password,
+    });
+
+    if (error) {
+      console.log(error);
+      alert("Пароль неверный");
+    } else {
+      router.replace("/");
+    }
   };
 
   return (
     <div className="flex items-center justify-center min-h-screen bg-gradient-to-r from-black via-[#1f2834] to-black px-4">
-      <div className="w-full max-w-md p-8 bg-black bg-opacity-90 rounded-xl shadow-2xl
+      <div
+        className="w-full max-w-md p-8 bg-black bg-opacity-90 rounded-xl shadow-2xl
                       transform transition-transform duration-500 hover:scale-105
-                      border border-gray-700">
+                      border border-gray-700"
+      >
         <h1 className="text-4xl text-center text-white mb-8 animate-pulse">
           Вход
         </h1>
         <form className="space-y-6" onSubmit={handleSubmit}>
           <div>
-            <label htmlFor="email" className="block text-sm font-semibold text-gray-300 mb-1">
+            <label
+              htmlFor="email"
+              className="block text-sm font-semibold text-gray-300 mb-1"
+            >
               Email
             </label>
             <input
@@ -25,6 +53,8 @@ export default function LogIn() {
               name="email"
               type="email"
               required
+              value={formData.email}
+              onChange={handleInputChange}
               className="block w-full px-4 py-2 mt-1 text-white bg-gray-700 border border-gray-600 rounded
                          focus:border-white focus:ring focus:ring-white focus:outline-none
                          transition-colors duration-300"
@@ -33,7 +63,10 @@ export default function LogIn() {
             />
           </div>
           <div>
-            <label htmlFor="password" className="block text-sm font-semibold text-gray-300 mb-1">
+            <label
+              htmlFor="password"
+              className="block text-sm font-semibold text-gray-300 mb-1"
+            >
               Пароль
             </label>
             <input
@@ -41,6 +74,8 @@ export default function LogIn() {
               name="password"
               type="password"
               required
+              value={formData.password}
+              onChange={handleInputChange}
               className="block w-full px-4 py-2 mt-1 text-white bg-gray-700 border border-gray-600 rounded
                          focus:border-blue-500 focus:ring focus:ring-blue-400 focus:outline-none
                          transition-colors duration-300"
@@ -49,7 +84,10 @@ export default function LogIn() {
             />
           </div>
           <div className="flex items-center justify-between">
-            <Link href="/forget" className="text-xs text-blue-400 hover:underline">
+            <Link
+              href="/forget"
+              className="text-xs text-blue-400 hover:underline"
+            >
               Забыли пароль?
             </Link>
           </div>
@@ -64,7 +102,10 @@ export default function LogIn() {
         </form>
         <p className="mt-6 text-sm text-center text-gray-300">
           Нет аккаунта?{" "}
-          <Link href="./signUp" className="font-medium text-blue-400 hover:underline">
+          <Link
+            href="./signUp"
+            className="font-medium text-blue-400 hover:underline"
+          >
             Зарегистрироваться
           </Link>
         </p>
--- frontend/src/pages/signUp/index.tsx
+++ frontend/src/pages/signUp/index.tsx
@@ -1,27 +1,64 @@
-import Link from "next/link";
-import { FormEvent } from "react";
+import { ChangeEvent, FormEvent, useState } from "react";
 import "../../app/globals.css";
+import { useRouter } from "next/navigation";
+import { supabase } from "@/lib/supabaseClient";
 
 export default function index() {
-  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+  const router = useRouter();
+  const [formData, setFromData] = useState({
+    name: "",
+    email: "",
+    password: "",
+    passwordConfirm: "",
+  });
+
+  const handleInputChange = (e: ChangeEvent<HTMLInputElement>) =>
+    setFromData((prev) => ({ ...prev, [e.target.name]: e.target.value }));
+
+  const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
+
+    if (formData.passwordConfirm.trim().length === 0) {
+			return alert("Введите пароль");
+		}
+
+		if (formData.passwordConfirm != formData.password) {
+			return alert("Пароли не совпадают")
+		}
+
+		if (formData.email) {
+			const { error, data } = await supabase.auth.signUp({
+				email: formData.email,
+				password: formData.password,
+			});
+
+			if (error) {
+				console.error(error);
+			} else {
+				router.replace("/confirm");
+			}
+		}
   };
 
   return (
     <div className="flex items-center justify-center min-h-screen bg-gradient-to-r from-gray-950 via-gray-800 to-gray-950 px-4">
-      <div className="w-full max-w-md p-8 bg-gray-950 bg-opacity-90 rounded-xl shadow-2xl
+      <div
+        className="w-full max-w-md p-8 bg-gray-950 bg-opacity-90 rounded-xl shadow-2xl
                       transform transition-transform duration-500 hover:scale-105
-                      border border-gray-700">
+                      border border-gray-700"
+      >
         <h1 className="text-4xl text-center text-white mb-8 animate-pulse">
           Регистрация
         </h1>
         <form className="space-y-6" onSubmit={handleSubmit}>
-        <div>
+          <div>
             <input
               id="name"
               name="name"
               type="text"
               required
+              value={formData.name}
+              onChange={handleInputChange}
               className="block w-full px-4 py-2 mt-1 text-white bg-gray-700 border border-gray-600 rounded
                          focus:border-white focus:ring focus:ring-white focus:outline-none
                          transition-colors duration-300"
@@ -34,6 +71,8 @@ export default function index() {
               name="email"
               type="email"
               required
+              value={formData.email}
+              onChange={handleInputChange}
               className="block w-full px-4 py-2 mt-1 text-white bg-gray-700 border border-gray-600 rounded
                          focus:border-white focus:ring focus:ring-white focus:outline-none
                          transition-colors duration-300"
@@ -46,6 +85,8 @@ export default function index() {
               name="password"
               type="password"
               required
+              value={formData.password}
+              onChange={handleInputChange}
               className="block w-full px-4 py-2 mt-1 text-white bg-gray-700 border border-gray-600 rounded
                          focus:border-blue-500 focus:ring focus:ring-blue-400 focus:outline-none
                          transition-colors duration-300"
@@ -58,6 +99,8 @@ export default function index() {
               name="passwordConfirm"
               type="password"
               required
+              value={formData.passwordConfirm}
+              onChange={handleInputChange}
               className="block w-full px-4 py-2 mt-1 text-white bg-gray-700 border border-gray-600 rounded
                          focus:border-blue-500 focus:ring focus:ring-blue-400 focus:outline-none
                          transition-colors duration-300"
