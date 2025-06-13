6b0ced15389ee5e7e1ff442a5b80b3886c55e5ed
SuNetVerseWolk <volk333333333@gmail.com>
2025-06-14 01:42:02 +0300

    Update Card.tsx

--- frontend/src/components/Card.tsx
+++ frontend/src/components/Card.tsx
@@ -4,19 +4,21 @@ import { Cards } from "../../generated/prisma";
 import { useDeleteCard, useUpdateCard } from "@/hooks/useCards";
 
 const Card: React.FC<Cards> = ({ ...params }) => {
-  const [card, setCard] = useState<Omit<Cards, 'id' | 'created_at'>>({
+  const [card, setCard] = useState<Omit<Cards, "id" | "created_at">>({
     text: params.text || "",
     translation: params.translation || "",
-		id_set: params.id_set
+    id_set: params.id_set,
   });
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState("");
   const updateCard = useUpdateCard(params.id);
   const deleteCard = useDeleteCard(params.id);
 
-	const handleChange = (e: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
-		setCard(prev => ({...prev, [e.target.name]: e.target.value}))
-	}
+  const handleChange = (
+    e: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+  ) => {
+    setCard((prev) => ({ ...prev, [e.target.name]: e.target.value }));
+  };
 
   const handleGenerateDefinition = async () => {
     if (!card.text?.trim()) {
@@ -37,7 +39,12 @@ const Card: React.FC<Cards> = ({ ...params }) => {
       if (!response.ok) throw new Error("Definition generation failed");
 
       const data = await response.json();
-      setCard((prev) => ({ ...prev, translation: data.definition }));
+      setCard((prev) => {
+        const result = { ...prev, translation: data.definition };
+        updateCard.mutate(result);
+
+        return result;
+      });
     } catch (err) {
       setError("Failed to generate definition");
       console.error(err);
@@ -46,9 +53,11 @@ const Card: React.FC<Cards> = ({ ...params }) => {
     }
   };
 
-	function handleBlur(event: React.FocusEvent<HTMLInputElement | HTMLTextAreaElement>): void {
-		updateCard.mutate(card);
-	}
+  function handleBlur(
+    event: React.FocusEvent<HTMLInputElement | HTMLTextAreaElement>
+  ): void {
+    updateCard.mutate(card);
+  }
 
   return (
     <div
@@ -67,8 +76,8 @@ const Card: React.FC<Cards> = ({ ...params }) => {
         placeholder="Термин"
         value={card.text!}
         onChange={handleChange}
-				onBlur={handleBlur}
-				disabled={deleteCard.isPending}
+        onBlur={handleBlur}
+        disabled={deleteCard.isPending}
       />
       <textarea
         id="translation"
@@ -79,8 +88,8 @@ const Card: React.FC<Cards> = ({ ...params }) => {
         placeholder="Определение"
         value={card.translation!}
         onChange={handleChange}
-				onBlur={handleBlur}
-				disabled={deleteCard.isPending}
+        onBlur={handleBlur}
+        disabled={deleteCard.isPending}
       />
 
       {error && <p className="text-red-500 text-sm mt-2">{error}</p>}
@@ -91,8 +100,8 @@ const Card: React.FC<Cards> = ({ ...params }) => {
                         hover:bg-red-700 transition-colors duration-300 shadow-md
                         focus:outline-none focus:ring-2 focus:ring-red-500 focus:ring-offset-2 
                         py-1 px-2 sm:py-1.5 sm:w-20 rounded-sm flex-1 sm:flex-none"
-												onClick={() => deleteCard.mutate()}
-												disabled={deleteCard.isPending}
+          onClick={() => deleteCard.mutate()}
+          disabled={deleteCard.isPending}
         >
           Удалить
         </button>
