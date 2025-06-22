e055a2eecd946de02b7449df25622a0b0cf205a3
Kirill <kirillbanhenco@gmail.com>
2025-06-22 18:44:06 +0300

    corrected style

--- frontend/src/app/set/[id]/page.tsx
+++ frontend/src/app/set/[id]/page.tsx
@@ -80,8 +80,8 @@ const Page = () => {
   };
 
   return (
-    <div className="flex justify-center min-h-screen bg-gradient-to-r from-black via-[#1f2834] to-black px-4 py-8">
-      <div className="w-full max-w-4xl space-y-4">
+    <div className="flex justify-center min-h-screen bg-gradient-to-r from-black via-[#1f2834] to-black px-4">
+      <div className="w-lg h-full">
         <UserSetItem />
 
         <button
@@ -90,25 +90,25 @@ const Page = () => {
           className="text-white font-semibold bg-blue-900 hover:bg-blue-700 
                     transition-colors duration-300 shadow-md focus:outline-none w-full
                     focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 
-                    py-2 px-4 rounded-sm text-base
-                    disabled:opacity-50 disabled:cursor-not-allowed"
+                    py-1.5 sm:py-2 px-4 rounded-sm text-sm sm:text-base flex-1 text-center
+                    disabled:opacity-50 disabled:cursor-not-allowed my-2"
         >
           {isAdding
-            ? "Processing..."
+            ? "Добавляется..."
             : isMine
-            ? "Add Card"
-            : "Copy Set to My Collection"}
+            ? "Добавить карточку"
+            : "Добавить набор к себе"}
         </button>
 
-        <div className="flex flex-col bg-white/5 p-5 rounded-2xl space-y-4">
+        <div className="flex flex-col bg-white/5 p-5 min-h-50 rounded-2xl overflow-auto">
           {isLoading ? (
-            <p className="text-gray-400 text-center py-4">Loading cards...</p>
+            <p className="text-gray-400 text-center py-4">Загрузка карточек...</p>
           ) : error ? (
             <p className="text-red-500 text-center py-4">
-              Error loading cards: {error.message}
+              Ошибка загрузки: {error.message}
             </p>
           ) : set?.Cards?.length === 0 ? (
-            <p className="text-gray-400 text-center py-4">No cards yet</p>
+            <p className="text-gray-400 text-center py-4">Пусто</p>
           ) : (
             set?.Cards?.map((card) => (
               <Card key={card.id.toString()} {...card} />
